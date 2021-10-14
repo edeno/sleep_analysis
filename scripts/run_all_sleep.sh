8702934af4d01cad1598bc8f4a5cd24830bdd2a6
Eric Denovellis <edeno@bu.edu>
2021-10-14 10:49:40 -0400

    Don't do sorted spikes for now

--- scripts/run_all_sleep.sh
+++ scripts/run_all_sleep.sh
@@ -15,4 +15,4 @@ python queue_sleep_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 2 --data_typ
 python queue_sleep_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 --data_type 'clusterless' --n_cores $NCORES --n_workers $NWORKERS --wall_time $REMY_WALLTIME
 
 # Sorted Spikes
-python queue_sleep_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME
+# python queue_sleep_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME
