4749fdb88913381e824ecbf84be046780598fe13
Eric Denovellis <edeno@bu.edu>
2021-10-15 11:17:50 -0400

    Run sorted spikes

--- scripts/run_all_sleep.sh
+++ scripts/run_all_sleep.sh
@@ -56,4 +56,4 @@ python queue_sleep_cluster_jobs.py --Animal 'remy' --Day 37 --Epoch 4 \
 
 
 # Sorted Spikes
-# python queue_sleep_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME
+python queue_sleep_cluster_jobs.py --data_type 'sorted_spikes' --n_cores $NCORES --n_workers $NWORKERS --wall_time $SORTED_WALLTIME
